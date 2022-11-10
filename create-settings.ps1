bf56351e73c8529dec500cd882d99e8368a926d6
Oskar Bengtsson <oskar@tensify.se>
2022-11-10 15:03:35 +0100

    Added create-settings.ps1

---
+++ create-settings.ps1
@@ -0,0 +1,19 @@
+# This script creates a file called settings.json used in other scripts
+# Ask for...
+
+# Username
+# Full name
+# Firstname
+# Surname
+# Displayname
+# Mobile
+
+# SMTP-server FQDN
+# SMTP-server port
+# Use SSL (True or false)
+# Email FROM-address
+# Recipient addresses
+# SMTP-username
+# SMTP-password
+
+# Save anything that has been added, omit everything else.
