d07789fb1b8743339299af914b90bde91c72f352
Oskar <oskar@tensify.se>
2022-11-16 12:53:52 +0100

    Updated

--- check-filehash-keepass.ps1
+++ check-filehash-keepass.ps1
@@ -1,1 +1,8 @@
 # This script checks the file hash for keepass against the txt record from hash.webland.se
+$ValidHash= ""
+$ValidHash= "BC86C00BE3A725DFAECC6144A5B1C9FD4A821AE24A86AB00E9EAFD318FB6DE69"
+if ($ValidHash -eq (Get-FileHash -Path \\path\to\the\installer.exe).Hash){
+    Write-Host "Valid Keepass-installer" -ForegroundColor Green
+}else {
+    Write-Host "Invalid KeePass-installer. Check your source!" -ForegroundColor Red
+}
